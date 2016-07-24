47199997053764a99180433fde6e309c474d9b00
ktenman <ktenman@users.noreply.github.com>
2016-07-24 16:23:07 +0300

    Refactoring.

--- src/main/java/ee/icefire/decathlon/Decathlon.java
+++ src/main/java/ee/icefire/decathlon/Decathlon.java
@@ -16,7 +16,7 @@ class Decathlon {
 
 	Decathlon(String fileName) {
 		this.fileName = fileName;
-		getAthleteResultsFromFile();
+		getResultsOfAthleteFromFile();
 		if (validDataFound()) {
 			calculatePointsOfAthletes();
 			printResults();
@@ -25,7 +25,7 @@ class Decathlon {
 		}
 	}
 
-	private void getAthleteResultsFromFile() {
+	private void getResultsOfAthleteFromFile() {
 		try {
 			athleteResults = Files.lines(Paths.get(fileName)).collect(toList());
 		} catch (IOException e) {
