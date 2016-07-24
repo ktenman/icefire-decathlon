a3f646453d6439e3336754b48ef482353b163892
ktenman <ktenman@users.noreply.github.com>
2016-07-24 15:35:19 +0300

    Refactoring.

--- src/main/java/ee/icefire/decathlon/Decathlon.java
+++ src/main/java/ee/icefire/decathlon/Decathlon.java
@@ -20,8 +20,50 @@ class Decathlon {
 		} catch (IOException e) {
 			System.out.println("Something is wrong with input file.");
 		}
+		if (notValidDataFound()) {
+			System.exit(0);
+		}
 		calculatePoints(athleteResults);
 		printResults();
+	}
+
+	private boolean notValidDataFound() {
+		StringBuilder error = new StringBuilder("Please check your file.\n\nRequirments:\n");
+		boolean notValidDataFound = false;
+		if (noResultFound()) {
+			error.append("* There must be at least one result.\n");
+			notValidDataFound = true;
+		}
+		if (resultInWrongFormatExists()) {
+			error.append("* There is a result in wrong format.\n");
+			notValidDataFound = true;
+		}
+		if (notValidDataFound) {
+			System.out.print(error);
+		}
+		return notValidDataFound;
+	}
+
+	private boolean resultInWrongFormatExists() {
+//		Nimi;10,2;790;18,75;217;46,00;13,70;57,50;478;81,00;3:40,2
+		for (String athleteResult : athleteResults) {
+			String[] columns = athleteResult.split(";");
+			if (columns.length != 11) {
+				return true;
+			}
+			for (int i = 1; i < columns.length; i++) {
+				try {
+					PerformanceParser.parsePerformanceToFloat(columns[i]);
+				} catch (Exception e) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean noResultFound() {
+		return athleteResults.size() == 0;
 	}
 
 	private void calculatePoints(List<String> athleteResults) {
--- src/main/java/ee/icefire/decathlon/PerformanceParser.java
+++ src/main/java/ee/icefire/decathlon/PerformanceParser.java
@@ -5,9 +5,7 @@ class PerformanceParser {
 		value = value.replace(",", ".");
 		if (value.contains(":")) {
 			String[] values = value.split(":");
-
 			return Float.parseFloat(values[0]) * 60 + Float.parseFloat(values[1]);
-
 		}
 		return Float.parseFloat(value);
 	}
